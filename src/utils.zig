7f2e817e30e85deee50b06497e47aed391da9af3
ethnatpf <ethnatscolaire@gmail.com>
2026-07-25 08:35:05 -0400

    Add a comment to the decimalLength utilk

--- src/utils.zig
+++ src/utils.zig
@@ -1,3 +1,4 @@
+/// Hard-coded for 3 digits max. Could be improved but works for our use case.
 pub fn decimalLength(value: u8) usize {
     if (value < 10) return 1;
     if (value < 100) return 2;
